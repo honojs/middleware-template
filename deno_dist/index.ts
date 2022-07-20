7ab697fd6468a876ec44eafe96541f3a26ddd4bf
Yusuke Wada <yusuke@kamawada.com>
2022-07-20 22:16:31 +0900

    add tests

--- deno_dist/index.ts
+++ deno_dist/index.ts
@@ -1,6 +1,6 @@
-import { Context, Next } from 'https://deno.land/x/hono@v1.6.3/mod.ts'
+import { Context, Next, Handler } from 'https://raw.githubusercontent.com/honojs/hono/v2.0.2/deno_dist/mod.ts'
 
-export const hello = (message: string = 'Hello') => {
+export const hello = (message: string = 'Hello'): Handler => {
   return async (c: Context, next: Next) => {
     await next()
     c.res.headers.append('X-Message', message)
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { Context, Next } from 'hono'
+import { Context, Next, Handler } from 'hono'
 
-export const hello = (message: string = 'Hello') => {
+export const hello = (message: string = 'Hello'): Handler => {
   return async (c: Context, next: Next) => {
     await next()
     c.res.headers.append('X-Message', message)
